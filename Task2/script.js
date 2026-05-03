4898da2e6611406195dcad60ed2544c311062d03
sab2407 <sabareeshsivakumar88@gmail.com>
2026-05-03 22:50:42 +0530

    Create script.js

---
+++ Task2/script.js
@@ -0,0 +1,23 @@
+function showHome() {
+    alert("Home Page");
+}
+
+function showJobs() {
+    alert("Jobs Page");
+}
+
+function showEmployerDashboard() {
+    alert("Employer Dashboard");
+}
+
+function showCandidateDashboard() {
+    alert("Candidate Dashboard");
+}
+
+function showLogin() {
+    alert("Login Page");
+}
+
+function logout() {
+    alert("Logged out");
+}
